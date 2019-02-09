631ab3b6c267746bfa57b35e93d42655bf53c9c6
Char <charliusb@gmail.com>
2019-02-09 09:31:00 -0600

    Cyrus we need you here. Wear red!

---
+++ src/main/java/frc/robot/cyrus.java
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * Add your docs here.
+ */
+public class cyrus {
+    public  int B      ; {
+
+
+
+    if(1 == B)   {
+        B = 1         /*trololololololol*/                                                                             ;
+    } else if(2 == B) {
+        B = 2         /*lolololololololo*/                                                                            ;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    ///////////// Cyrus we need you! ///////////////
+}}
